469ff7faad1ab261c4782db4f8bfaf622b5c6e21
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-05-14 16:05:15 -0400

    good enough

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function lol() {
     const pass_text = document.getElementById("pass-text");
-    const pass_length = document.getElementById("char_length_input").value;
-    console.log(pass_length);
     let pass = "";
     const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_+-={}[]|;:'<>?,./";
-    for(let i = 0; i < pass_length; i++) {
+    for(let i = 0; i < 64; i++) {
         //
         let rand = Math.floor(Math.random() * characters.length);
         pass += characters.substring(rand, rand + 1);
     }
     pass_text.innerText = pass;
 }
+
+window.addEventListener('load', lol)
